a262f6bb545376126dbedc6703a466b716903130
yuta.sano <snyt45@gmail.com>
2021-05-18 16:33:57 +0900

    設定ファイル追加

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
 module.exports = {
-  lintOnSave: false,
+  configureWebpack: {
+    plugins: [
+      // ホットリロード時に stylelint を実行する
+      new StyleLintPlugin({
+        files: ['src/**/*.{vue,scss}'],
+      }),
+    ],
+  },
 };
