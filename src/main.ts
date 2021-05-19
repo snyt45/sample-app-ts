6990f1baff380eb3984eb34a764e837c74bfde69
yuta.sano <snyt45@gmail.com>
2021-05-19 13:33:27 +0900

    Vuetify導入

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import Vue from 'vue';
+import VueCompositionAPI from '@vue/composition-api';
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
-import VueCompositionAPI from '@vue/composition-api';
+import vuetify from '@/plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
+Vue.use(VueCompositionAPI);
 
 Vue.config.productionTip = false;
-Vue.use(VueCompositionAPI);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
