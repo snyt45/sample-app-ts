b7b76772bf93d0bd50c2cdce2db71a8ff4fddc14
yuta.sano <snyt45@gmail.com>
2021-05-19 09:09:41 +0900

    第3章 STEP9

---
+++ src/views/Calendar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>カレンダー[{{ type }}]</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    type: {
+      required: true,
+      validator: (value: string) => {
+        return ['month', 'week', 'day'].includes(value);
+      },
+    },
+  },
+});
+</script>
