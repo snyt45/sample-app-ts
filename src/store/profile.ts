f2684b5098c7ae2b6caf04f3d6ae7aacf5a12af8
yuta.sano <snyt45@gmail.com>
2021-05-20 18:09:03 +0900

    STEP13

--- src/store/profile.ts
+++ src/store/profile.ts
@@ -1,16 +1,8 @@
 import '@/plugins/composition-api';
+import axios from 'axios';
 import { reactive } from '@vue/composition-api';
 import { Profile } from '@/store/profile.model';
 import { update } from '@/store/shared-user';
-
-export const profileMockData: Profile = {
-  userId: '66006b29-727e-4ed8-a3c8-95d4438f66d4',
-  userName: 'naminami',
-  nickname: '波平',
-  themeColor: '#4caf50',
-  hasAvatar: false,
-  mailAddress: 'namihei@example.com',
-};
 
 export const profileStore = reactive({ profile: null as Profile | null });
 
@@ -18,13 +10,21 @@ export const profileStore = reactive({ profile: null as Profile | null });
  * ユーザー名を更新します。
  * @param userName ユーザー名
  */
-export const updateUserName = (userName: string) => {
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const updateUserNameAsync = async (userName: string) => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  profileStore.profile!.userName = userName;
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  update(profileStore.profile!);
-};
+  const profile = profileStore.profile!;
+  const data = { userName: userName };
 
+  // eslint-disable-next-line no-useless-catch
+  try {
+    await axios.patch('profile', data);
+    profile.userName = userName;
+    update(profile);
+  } catch (error) {
+    throw error;
+  }
+};
 /**
  * ニックネームを更新します。
  * @param nickname ニックネーム
@@ -45,4 +45,20 @@ export const updateThemeColor = (themeColor: string) => {
   profileStore.profile!.themeColor = themeColor;
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   update(profileStore.profile!);
+};
+
+/**
+ * サインインします。
+ */
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const signInAsync = async () => {
+  // ここに外部認証に関連した実装が必要
+
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const response = await axios.get<Profile>('profile');
+    profileStore.profile = response.data;
+  } catch (error) {
+    throw error;
+  }
 };
